22d5a4fc8cf55c4f470a80aaf25294daa8857aeb
Alan Ramirez Herrera <alan5142@hotmail.com>
2023-09-23 13:29:07 -0600

    feat: Added login and sign in page

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:proyecto/cafes.dart';
 import 'package:provider/provider.dart';
 import 'package:proyecto/main_provider.dart';
+import 'package:proyecto/pages/login_page.dart';
+import 'package:proyecto/pages/sign_in_page.dart';
 
-void main() => runApp( MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-   MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Cafeterias App',
-      home: Scaffold(
-        appBar: AppBar(
-          title:  Text('Cafeterias App'),
-        ),
-        body:  ChangeNotifierProvider(child: Cafes(), create: (context) => MainProvider(),)
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: SignInPage(),
     );
   }
 }
